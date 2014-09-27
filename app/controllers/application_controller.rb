0edb610ebbb8fff8dd662e86321e459e16511377
Y-Fujikawa <fujikawa0@gmail.com>
2014-09-27 22:42:10 +0900

    ログイン処理とbootstrapの適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    user_session_path
+  end
 end
